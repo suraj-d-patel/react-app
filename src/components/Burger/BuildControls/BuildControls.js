d61484bdc074785b4a5643a18350fedd00ff22ca
“suraj-d-patel” <user.surajdpatel@gmail.com>
2019-12-02 20:37:56 +0530

    initialize the react project

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import classes from './BuildControls.css'
+import BuildControl from './BuildControl/BuildControl';
+
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' }
+]
+
+const buildControls = props => (
+
+    <div className={classes.BuildControls}>
+        {controls.map(ctrl => (
+            <BuildControl 
+            key={ctrl.label} 
+            // type={ctrl.type}
+            disabled={props.disabled[ctrl.type]}
+            removed={()=>props.ingredientRemoved(ctrl.type)}
+            added={()=>props.ingredientAdded(ctrl.type)}
+            label={ctrl.label} />
+        ))}
+    </div>
+);
+
+export default buildControls;
+
